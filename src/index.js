f077e640511eabb93b183a6f70c65f7f300314b8
+ <+>
2024-09-16 21:06:01 -0500

    just have two more test to fix

--- src/index.js
+++ src/index.js
@@ -5,40 +5,37 @@ const handleClick = (ramen) => {
   document.querySelector('.name').textContent = ramen.name;
   document.querySelector('.detail-image').src = ramen.image;
   document.querySelector('.restaurant').textContent = ramen.restaurant;
-  document.getElementById('rating-display').textContent = `Rating: ${ramen.rating}`;
-  document.getElementById('comment-display').textContent = `Comment: ${ramen.comment}`;
+  document.getElementById('rating-display').textContent = ramen.rating;
+  document.getElementById('comment-display').textContent = ramen.comment;
 }
 
 const addSubmitListener = () => {
   const form = document.getElementById('new-ramen');
-
-  form.addEventListener('submit', ramen => {
-    ramen.preventDefault();
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
     const newRamen = {
       name: form.name.value,
       restaurant: form.restaurant.value,
       image: form.image.value,
-      rating: form['new-ramen-rating'].value,
-      comment: form['new-ramen-comment'].value
+      rating: form['new-rating'].value,
+      comment: form['new-comment'].value
     };
 
     addNewRamenToMenu(newRamen);
-
     form.reset();
-  });
+  })
 }
 
-const addNewRamenToMenu(ramen) {
+const addNewRamenToMenu = (ramen)  => {
   const ramenMenu = document.getElementById('ramen-menu');
   const img = document.createElement('img');
   img.src = ramen.image;
   img.alt = ramen.name;
 
   img.addEventListener('click', () => handleClick(ramen));
-
   ramenMenu.appendChild(img);
-}
+};
 
 const displayRamens = () => {
   fetch('http://localhost:3000/ramens')
@@ -53,10 +50,11 @@ const displayRamens = () => {
       img.alt = ramen.name;
       img.addEventListener('click', () => handleClick(ramen));
       ramenMenu.appendChild(img);
-    })
+    });
   })
-  .catch(error => console.log('Error fetching ramens:', error));
+  .catch(error => console.log('Error fecthing ramens:', error));
 }
+
 
 
 const main = () => {
